abe7a271067e104191ceca8e3da4b5a3060d052f
msstevers <msstevers@gmail.com>
2017-03-06 01:11:57 -0600

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'users#index'
+  
+  
 end
